9807989fa482214b98bf57b608ae87206e0a70cc
vhaalz <emmanuelagemerien10@gmail.com>
2025-02-28 20:12:42 +0100

    first commit

---
+++ src/test/java/com/vhaalz/task_manager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhaalz.task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
